e44d6d5eae6d0cd40dd3a1c366acdb08883b2483
Nitish Kumar <nitishkumartt@gmail.com>
2022-01-08 16:51:54 +0000

    added array utils

---
+++ src/Utils/ArrayUtils.java
@@ -0,0 +1,23 @@
+package Utils;
+
+import java.util.Scanner;
+
+public class ArrayUtils {
+    public static void printArray(int arr[]){
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+    public static int[] getArrayInt(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = scanner.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter array values");
+        for (int i=0;i<size;i++){
+            arr[i]=scanner.nextInt();
+        }
+        return arr;
+    }
+}
