48d3c7136f582001aa010a9879c235f5f3adaaf9
Nitish Kumar <nitishkumartt@gmail.com>
2022-01-13 20:41:36 +0000

    Array |  order agnostic binary search

---
+++ src/Arrays/OrderAgnosticBinarySearch.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {423,300,235,215,54,36,7,-8,-43};
+        System.out.println(getElementIndex(arr, 235));
+    }
+
+    public static int getElementIndex(int[] arr, int targetElement) {
+        int startIndex = 0;
+        int endIndex = arr.length - 1;
+        while (startIndex <= endIndex) {
+            /*
+             * int midIndex = (startIndex+endIndex)/2;
+             * start+end might have large value which might not fit in integer range
+             */
+            int midIndex = startIndex + (endIndex - startIndex) / 2;
+            if (arr[midIndex] == targetElement)
+                return midIndex;
+            boolean isAsc = arr[startIndex] < arr[endIndex];
+            if (isAsc) {
+                if (arr[midIndex] < targetElement)
+                    startIndex = midIndex + 1;
+                else if (arr[midIndex] > targetElement)
+                    endIndex = midIndex - 1;
+            } else {
+                if(arr[midIndex]< targetElement)
+                    endIndex =midIndex-1;
+                else if(arr[midIndex]> targetElement)
+                    startIndex =midIndex+1;
+            }
+        }
+        return -1;
+    }
+}
