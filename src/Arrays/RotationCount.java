2040256b3532cfd47c3ff78a3449c868b75a9a07
Nitish Kumar <nitishkumartt@gmail.com>
2022-01-26 18:04:57 +0000

    Rotated Sorted Array Rotation Count

---
+++ src/Arrays/RotationCount.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr = {8,9,12,2,3,5,7};
+        System.out.println(getRotationCount(arr));
+    }
+
+    //https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
+    private static int getRotationCount(int[] arr) {
+        return SearchRotatedArray.getPivot(arr)+1;
+    }
+}
