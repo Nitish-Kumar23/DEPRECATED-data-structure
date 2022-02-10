43714bffbc0379cddefa5f5649acd1d05c978602
Nitish Kumar <nitishkumartt@gmail.com>
2022-02-10 21:04:30 +0000

    1342 | Number of steps

---
+++ src/RecursionQA/NumberOfSteps.java
@@ -0,0 +1,20 @@
+package RecursionQA;
+
+public class NumberOfSteps {
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(0));
+    }
+
+    private static int numberOfSteps(int num) {
+        if(num==0){
+            return 0;
+        }
+        if(num==1){
+            return 1;
+        }
+        if(num%2==0){
+            return 1+numberOfSteps(num/2);
+        }
+        return 1+numberOfSteps(num-1);
+    }
+}
