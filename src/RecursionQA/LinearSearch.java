2477e9175ba9d82341f0ceec39f426988fca0478
Nitish Kumar <nitishkumartt@gmail.com>
2022-02-11 19:22:26 +0000

    Recursion | Linear search

--- src/RecursionQA/LinearSearch.java
+++ src/RecursionQA/LinearSearch.java
@@ -14,6 +14,7 @@ public class LinearSearch {
         findAllIndices(arr, target, 0);
         System.out.println(result);
         System.out.println(findAllIndices(arr, target, 0, new ArrayList<>()));
+        System.out.println(findAllIndicesV2(arr, target, 0));
 
     }
 
@@ -87,7 +88,7 @@ public class LinearSearch {
 
     /**
      * returns an arraylist of multiple occurrence of indexes
-     * 
+     *
      * @param arr
      * @param target
      * @param index
@@ -104,6 +105,31 @@ public class LinearSearch {
         }
         //we are passing output arraylist in recursion call and adding indexes in it
         return findAllIndices(arr, target, index + 1, output);
+    }
+
+    /**
+     * return arraylist of indexes of target element in array
+     * NOT OPTIMIZED APPROACH
+     *
+     * @param arr
+     * @param target
+     * @param index
+     * @return
+     */
+    private static ArrayList<Integer> findAllIndicesV2(int[] arr, int target, int index) {
+        //creating arraylist in each recursion call
+        ArrayList<Integer> list = new ArrayList<>();
+        if (arr.length == index) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> list2 = findAllIndicesV2(arr, target, index + 1);
+        if (!list.isEmpty()) {
+            list2.add(list.get(0));
+        }
+        return list2;
     }
 
 }
