35ad0037d07281c88b26b235f68f29f58e81c5ac
Nitish Kumar <nitishkumartt@gmail.com>
2022-02-14 20:55:55 +0000

    Doubly linked list

--- src/DS/DoublyLinkedList.java
+++ src/DS/DoublyLinkedList.java
@@ -1,34 +1,37 @@
 package DS;
 
-import java.util.LinkedList;
-
 public class DoublyLinkedList {
-    
+
     private static DoublyLinkedListNode head = null;
     private static DoublyLinkedListNode tail = null;
     private static int size = 0;
+
     public static void main(String[] args) {
         DoublyLinkedList list = new DoublyLinkedList();
-        list.addNodeAtHead(2);
-        list.addNodeAtHead(3);
-        list.addNodeAtHead(8);
+        addNodeAtHead(2);
+        addNodeAtHead(3);
+        addNodeAtHead(8);
         printLinkedList();
         printLinkedListRev(getLastNode());
+        addAtIndex(1, 17);
+        addAtIndex(4, 19);
+        addAtIndex(0, 11);
+        printLinkedList();
     }
 
     private static void printLinkedListRev(DoublyLinkedListNode lastNode) {
         System.out.print("Print in reverse : ");
         DoublyLinkedListNode node = lastNode;
-        while (node!=null){
-            System.out.print(node.data+"->");
+        while (node != null) {
+            System.out.print(node.data + "->");
             node = node.prev;
         }
         System.out.println("END");
     }
 
-    public static DoublyLinkedListNode getLastNode(){
+    public static DoublyLinkedListNode getLastNode() {
         DoublyLinkedListNode node = head;
-        while (node.next!=null){
+        while (node.next != null) {
             node = node.next;
         }
         return node;
@@ -36,21 +39,50 @@ public class DoublyLinkedList {
 
     private static void printLinkedList() {
         DoublyLinkedListNode node = head;
-        while (node!=null){
-            System.out.print(node.data+"->");
-            node=node.next;
+        while (node != null) {
+            System.out.print(node.data + "->");
+            node = node.next;
         }
         System.out.println("END");
     }
 
-    private void addNodeAtHead(int value) {
+    private static void addNodeAtHead(int value) {
         DoublyLinkedListNode node = new DoublyLinkedListNode(value);
-        if(head!=null) {
+        if (head != null) {
             node.prev = head.prev;
-            head.prev =node;
+            head.prev = node;
         }
         node.next = head;
         head = node;
         size++;
+    }
+
+    public static void addAtIndex(int index, int val) {
+
+        if (index > size)
+            return;
+
+        if (index == 0) {
+            addNodeAtHead(val);
+            return;
+        }
+
+        if (index == size) {
+            tail = getLastNode();
+            DoublyLinkedListNode node = new DoublyLinkedListNode(val, tail, tail.next);
+            tail.next = node;
+            tail = node;
+            return;
+        }
+
+        DoublyLinkedListNode temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.getNext();
+        }
+        DoublyLinkedListNode node = new DoublyLinkedListNode(val, temp, temp.next);
+        temp.next = node;
+        node.next.prev = node;
+        temp.setNext(node);
+        size += 1;
     }
 }
