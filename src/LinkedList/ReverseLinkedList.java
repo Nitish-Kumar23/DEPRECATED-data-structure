912833eec0e762dafbcea320e697c757433888bc
Nitish Kumar <nitishkumartt@gmail.com>
2022-02-15 17:33:50 +0000

    206. Reverse Linked List

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,63 @@
+package LinkedList;
+
+import DS.MyLinkedList;
+
+import java.util.Stack;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+        list.addAtHead(1);
+        list.addAtTail(2);
+        list.addAtTail(3);
+        list.addAtTail(4);
+        list.addAtTail(5);
+        MyLinkedList.printLinkedList();
+//        printLL(reverseLL(MyLinkedList.getHead()));
+        printLL(reverseLLV2(MyLinkedList.getHead()));
+
+    }
+
+    private static void printLL(MyLinkedList.LinkedListNode head) {
+        MyLinkedList.LinkedListNode node = head;
+        while (node!=null){
+            System.out.print(node.getValue()+"->");
+            node = node.getNext();
+        }
+        System.out.println("END");
+    }
+
+    private static MyLinkedList.LinkedListNode reverseLL(MyLinkedList.LinkedListNode head) {
+        Stack<MyLinkedList.LinkedListNode> stack = new Stack<>();
+        MyLinkedList.LinkedListNode node = head;
+        while (node!=null){
+            stack.push(node);
+            node = node.getNext();
+        }
+        MyLinkedList.LinkedListNode reverseLLHead = null;
+        MyLinkedList.LinkedListNode cNode = null;
+        while (!stack.isEmpty()){
+            if(reverseLLHead==null){
+                reverseLLHead = stack.pop();
+                cNode = reverseLLHead;
+                continue;
+            }
+            cNode.setNext(stack.pop());
+            cNode = cNode.getNext();
+        }
+        cNode.setNext(null);
+        return reverseLLHead;
+    }
+
+    private static MyLinkedList.LinkedListNode reverseLLV2(MyLinkedList.LinkedListNode head) {
+        MyLinkedList.LinkedListNode current = head;
+        MyLinkedList.LinkedListNode prev = null;
+        while (current!=null){
+            MyLinkedList.LinkedListNode temp = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+}
