17a744f058ea7a6db458d5c492f9f9d7e1767aad
Nitish Kumar <nitishkumartt@gmail.com>
2022-02-25 19:56:47 +0000

    125. Valid Palindrome

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,29 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isValidPalindrome("0P"));
+    }
+
+    private static boolean isValidPalindrome(String input) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0;i<input.length();i++){
+            if(!Character.isLetterOrDigit(input.charAt(i))){
+                continue;
+            }
+            if(Character.isDigit(input.charAt(i))) {
+                result.append(input.charAt(i));
+            }
+            if(Character.isLetter(input.charAt(i))) {
+                result.append(Character.toLowerCase(input.charAt(i)));
+            }
+        }
+        String ans = result.toString();
+        int i=0,j=ans.length()-1;
+        while (i<j){
+            if(ans.charAt(i)!=ans.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
