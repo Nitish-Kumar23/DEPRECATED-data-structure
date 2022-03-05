ea1446aea9e11743fcba4d476b6d594dd1b4b3f5
Nitish Kumar <nitishkumartt@gmail.com>
2022-03-05 19:29:34 +0000

    refractor

---
+++ src/String/ASCII.java
@@ -0,0 +1,8 @@
+package String;
+
+public class ASCII {
+    public static void main(String[] args) {
+        char character = 'a';
+        System.out.println(character+0);
+    }
+}
---
+++ src/String/CaseCheck.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char input = scanner.next().trim().charAt(0);
+        System.out.println(input);
+
+        if (input >= 'a' && input <= 'z') {
+            System.out.println("Lowercase");
+        } else if (input >= 'A' && input <= 'Z') {
+            System.out.println("Uppercase");
+        }
+        variableLength(1, 42, 546, 24);
+    }
+
+    private static void variableLength(int... arguments) {
+        System.out.println(Arrays.toString(arguments));
+    }
+}
